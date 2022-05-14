52fc1d86facfd1536c1cb88b82df2e2a0e527bbc
Shou-Chi Chang <cckaries@gmail.com>
2022-05-14 23:32:19 +0800

    feat(data-structs): add singly linked list

---
+++ src/dataStructures/SinglyLinkedList.js
@@ -0,0 +1,107 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) { // T: O(1)
+    const node = new Node(value);
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+    return this.print();
+  }
+
+  prepend(value) { // T: O(1)
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+    return this.print();
+  }
+
+  insert(index = 0, value) { // T: O(n)
+    if(index === 0) {
+      this.prepend(value);
+      return this.print();
+    }
+
+    if(index >= this.length) {
+      return this.append(value);
+    }
+
+    const node = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = node;
+    node.next = follower;
+    this.length++;
+    return this.print();
+  }
+
+  delete(index) { // T: O(n)
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.print();
+  }
+
+  reverse() {
+    if(this.head.next === null) {
+      return this.head;
+    }
+
+    let firstNode = this.head;
+    let secondNode = firstNode.next;
+    while(secondNode) {
+      const tempNode = secondNode.next;
+      secondNode.next = firstNode;
+      firstNode = secondNode;
+      secondNode = tempNode;
+    }
+    this.tail = this.head;
+    this.head.next = null;
+    this.head = firstNode;
+    return this.print();
+  }
+
+  traverseToIndex(index) { // T: O(n)
+    let counter = 0;
+    let currNode = this.head;
+    while(counter !== index) {
+      currNode = currNode.next;
+      counter++;
+    }
+    return currNode;
+  }
+
+  print() { // T: O(n)
+    const res = [];
+    let currNode = this.head;
+    while(currNode !== null) {
+      res.push(currNode.value);
+      currNode = currNode.next;
+    }
+    console.log(res);
+    return res;
+  }
+}
+
+/* use cases */
+
+const myLinkedList = new SinglyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.delete(1);
+myLinkedList.reverse();
