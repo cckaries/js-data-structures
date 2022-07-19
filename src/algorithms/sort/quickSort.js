47f131c56df829dc10595ba9166dd592afc9dfa6
Shou-Chi Chang <cckaries@gmail.com>
2022-07-19 23:51:10 +0800

    refactor(quick-sort): update code

--- src/algorithms/sort/quickSort.js
+++ src/algorithms/sort/quickSort.js
@@ -2,39 +2,40 @@
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function quickSort(array, idxL, idxR){
+function quickSort(array, idxL, idxR) {
   let idxPivot;
   let idxPartition;
 
-  if(idxL < idxR) {
+  if (idxL < idxR) {
     idxPivot = idxR;
     idxPartition = partition(array, idxPivot, idxL, idxR);
-    
+
     //sort left and right
     quickSort(array, idxL, idxPartition - 1);
     quickSort(array, idxPartition + 1, idxR);
   }
   return array;
 }
-   
-function partition(array, idxPivot, idxL, idxR){
+
+function partition(array, idxPivot, idxL, idxR) {
   let valPivot = array[idxPivot];
   let idxPartition = idxL;
 
-  for(let i = idxL; i < idxR; i++) {
-    if(array[i] < valPivot){
+  for (let i = idxL; i < idxR; i++) {
+    if (array[i] < valPivot) {
       swap(array, i, idxPartition);
       idxPartition++;
     }
   }
+
   swap(array, idxR, idxPartition);
   return idxPartition;
 }
 
-function swap(array, idx1, idx2){
-    let temp = array[idx1];
-    array[idx1] = array[idx2];
-    array[idx2] = temp;
+function swap(array, idx1, idx2) {
+  let temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
 }
 
 //Select first and last index as 2nd and 3rd parameters
